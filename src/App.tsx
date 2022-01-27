b74eba33a66238022f23c06371aa6d085146703c
Charles Francoise <charles.francoise@payfit.com>
2022-01-28 00:50:49 +0100

    add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
+import { Box } from '@mui/material';
 
-const App = () => <h1>Hello World!</h1>;
+const App = () => <Box sx={{ typography: 'h1' }}>Hello World!</Box>;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
