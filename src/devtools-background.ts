7211cfe625e30228a0b2b9c19140d6e48c5bd7d7
Charles Francoise <charles.francoise@payfit.com>
2022-01-28 00:58:23 +0100

    configure eslint and prettier

--- src/devtools-background.ts
+++ src/devtools-background.ts
@@ -1,10 +1,1 @@
-chrome.devtools.panels.create(
-  'Interceptor',
-  '',
-  'static/index.html',
-  (panel) => {
-    panel.onShown.addListener((window) => {
-      console.log(chrome.devtools.panels);
-    });
-  }
-);
+chrome.devtools.panels.create('Interceptor', '', 'static/index.html');
