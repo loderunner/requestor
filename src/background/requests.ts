cb65eaa0b9f7cec9f19c58ea799b5306961b5149
Charles Francoise <charles.francoise@payfit.com>
2022-01-30 23:11:13 +0100

    move request listener to new module

---
+++ src/background/requests.ts
@@ -0,0 +1,83 @@
+import type Protocol from 'devtools-protocol'
+
+type TargetInfo = chrome.debugger.TargetInfo
+type Debuggee = chrome.debugger.Debuggee
+type RequestPausedEvent = Protocol.Fetch.RequestPausedEvent
+export type Request = Protocol.Network.Request
+
+class RequestEvent extends Event {
+  readonly request: Request
+  constructor(request: Request) {
+    super('request')
+    this.request = request
+  }
+}
+
+type RequestEventListener = (req: Request) => void
+
+class RequestEventTarget {
+  private callbacks = new Map<RequestEventListener, EventListener>()
+  private eventTarget = new EventTarget()
+
+  addListener(listener: RequestEventListener): void {
+    const callback = (event: Event) => {
+      const requestEvent = event as RequestEvent
+      listener(requestEvent.request)
+    }
+    this.callbacks.set(listener, callback)
+    this.eventTarget.addEventListener('request', callback)
+  }
+
+  removeListener(listener: RequestEventListener): void {
+    const callback = this.callbacks.get(listener)
+    if (callback !== undefined) {
+      this.eventTarget.removeEventListener('request', callback)
+    }
+  }
+
+  dispatchEvent(request: Request): boolean {
+    return this.eventTarget.dispatchEvent(new RequestEvent(request))
+  }
+}
+
+export const onRequest = new RequestEventTarget()
+
+const onDebuggerEvent = (
+  target: Debuggee,
+  method: string,
+  params?: unknown
+) => {
+  if (method === 'Fetch.requestPaused') {
+    const event: RequestPausedEvent = params as RequestPausedEvent
+    onRequest.dispatchEvent(event.request)
+    chrome.debugger.sendCommand(target, 'Fetch.continueRequest', {
+      requestId: event.requestId,
+    })
+  }
+}
+
+let debuggee: chrome.debugger.Debuggee
+
+const getTargets = async () => {
+  return new Promise<TargetInfo[]>((resolve) =>
+    chrome.debugger.getTargets((targets) => resolve(targets))
+  )
+}
+
+export const subscribe = async () => {
+  const targets = await getTargets()
+  for (const t of targets) {
+    if (t.tabId === chrome.devtools.inspectedWindow.tabId) {
+      debuggee = { targetId: t.id }
+      await chrome.debugger.attach(debuggee, '1.3')
+      chrome.debugger.onEvent.addListener(onDebuggerEvent)
+      await chrome.debugger.sendCommand(debuggee, 'Fetch.enable', {})
+    }
+  }
+}
+
+export const unsubscribe = () => {
+  chrome.debugger.sendCommand(debuggee, 'Fetch.disable', {})
+  chrome.debugger.onEvent.removeListener(onDebuggerEvent)
+  chrome.debugger.detach(debuggee)
+}
