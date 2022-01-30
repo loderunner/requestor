a7da5949be25eb27a1fdf6a18c22f501d94a7be5
Charles Francoise <charles.francoise@payfit.com>
2022-01-30 23:11:13 +0100

    move request listener to new module

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,56 +1,17 @@
-import type Protocol from 'devtools-protocol'
+import * as requests from './requests'
 
 chrome.devtools.panels.create('Interceptor', '', 'static/panel.html')
 
-type TargetInfo = chrome.debugger.TargetInfo
-type Debuggee = chrome.debugger.Debuggee
-type RequestPausedEvent = Protocol.Fetch.RequestPausedEvent
-
-const getTargets = async () => {
-  return new Promise<TargetInfo[]>((resolve) =>
-    chrome.debugger.getTargets((targets) => resolve(targets))
-  )
+const onRequest = (req: requests.Request) => {
+  const d = new Date()
+  console.log(`${d.toISOString()} - ${req.method} ${req.url}`)
 }
 
-const onDebuggerEvent = (
-  target: Debuggee,
-  method: string,
-  params?: unknown
-) => {
-  console.log('debugger event', method, params)
-  if (method === 'Fetch.requestPaused') {
-    const event: RequestPausedEvent = params as RequestPausedEvent
-    const d = new Date()
-    console.log(
-      `${d.toISOString()} - ${event.request.method} ${event.request.url}`
-    )
-    chrome.debugger.sendCommand(target, 'Fetch.continueRequest', {
-      requestId: event.requestId,
-    })
-  }
-}
-
-let debuggee: chrome.debugger.Debuggee
-
-const initDebugger = async () => {
-  const targets = await getTargets()
-  for (const t of targets) {
-    if (t.tabId === chrome.devtools.inspectedWindow.tabId) {
-      debuggee = { targetId: t.id }
-      if (t.attached) {
-        await chrome.debugger.attach(debuggee, '1.3')
-      }
-      chrome.debugger.onEvent.addListener(onDebuggerEvent)
-      await chrome.debugger.sendCommand(debuggee, 'Fetch.enable', {})
-    }
-  }
-}
-console.log(`${new Date().toISOString()} startup backup script`)
+requests.onRequest.addListener(onRequest)
 
 window.addEventListener('beforeunload', () => {
-  chrome.debugger.sendCommand(debuggee, 'Fetch.disable', {})
-  chrome.debugger.onEvent.removeListener(onDebuggerEvent)
-  chrome.debugger.detach(debuggee)
+  requests.onRequest.removeListener(onRequest)
+  requests.unsubscribe()
 })
 
-initDebugger()
+requests.subscribe()
