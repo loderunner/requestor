3813d2ce88d81f6fba2d83174889d2583463c09b
Charles Francoise <charles.francoise@payfit.com>
2022-01-31 14:09:17 +0100

    change request subscription API

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,15 +2,13 @@ import * as requests from './requests'
 
 chrome.devtools.panels.create('Interceptor', '', 'static/panel.html')
 
-const onRequest = (req: requests.Request) => {
+const unsubscribe = requests.subscribe((req: requests.Request) => {
   const d = new Date()
   console.log(`${d.toISOString()} - ${req.method} ${req.url}`)
-}
-
-requests.onRequest.addListener(onRequest)
+})
 
 window.addEventListener('beforeunload', () => {
-  requests.onRequest.removeListener(onRequest)
+  unsubscribe()
   requests.unlisten()
 })
 
