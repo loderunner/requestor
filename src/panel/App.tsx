31818a6665a885b0023d19caaca63794d12b758a
Charles Francoise <charles.francoise@payfit.com>
2022-02-02 14:04:57 +0100

    refactor: remove client-server model

--- src/panel/App.tsx
+++ src/panel/App.tsx
@@ -1,19 +1,35 @@
 import { Box, Stack } from '@mui/material'
 import * as React from 'react'
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
+
+import * as Requests from './requests'
 
 const App = () => {
-  const [requests] = useReducer(
-    (requests: string[], req: string) => [...requests, req],
+  const [requests, pushRequest] = useReducer(
+    (requests: Requests.Request[], req: Requests.Request) => [...requests, req],
     []
   )
+
+  useEffect(() => {
+    Requests.listen()
+    const unsubscribe = Requests.subscribe((req) => {
+      pushRequest(req)
+    })
+
+    return () => {
+      unsubscribe()
+      Requests.unlisten()
+    }
+  }, [])
 
   return (
     <>
       <Box sx={{ typography: 'h1' }}>Hello World!</Box>
       <Stack>
-        {requests.map((req, i) => (
-          <div key={i}>{req}</div>
+        {requests.map((request, i) => (
+          <div key={i}>
+            {request.method} {request.url}
+          </div>
         ))}
       </Stack>
     </>
