af196b55f7b1e0935bd452fb3b608fd1ffd0ea4f
Charles Francoise <charles.francoise@payfit.com>
2022-02-03 01:02:42 +0100

    add tests for requests module

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/// <reference types="jest-extended" />
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: [
     './node_modules/jest-enzyme/lib/index.js',
+    'jest-extended/all',
     './__setups__/chrome.setup.ts',
     './__setups__/enzyme.setup.ts',
   ],
---
+++ src/panel/__tests__/requests.ts
@@ -0,0 +1,66 @@
+import { chrome } from 'jest-chrome'
+
+import * as Requests from '../requests'
+
+const target = {
+  attached: false,
+  id: 'target',
+  title: 'Title',
+  type: 'targetType',
+  url: 'https://example.com',
+}
+
+describe('[Requests]', () => {
+  beforeEach(async () => {
+    chrome.debugger.onEvent.clearListeners()
+  })
+
+  it('should crash without targets', async () => {
+    chrome.debugger.getTargets.mockImplementation((callback) => callback([]))
+
+    await expect(Requests.listen()).rejects.toThrow()
+  })
+
+  it('should add a listener to debugger events', async () => {
+    chrome.debugger.getTargets.mockImplementation((callback) =>
+      callback([target])
+    )
+
+    await Requests.listen()
+
+    expect(chrome.debugger.onEvent.hasListeners()).toBeTrue()
+  })
+
+  it('should remove the listener from debugger events', async () => {
+    chrome.debugger.getTargets.mockImplementation((callback) =>
+      callback([target])
+    )
+
+    await Requests.listen()
+    await Requests.unlisten()
+
+    expect(chrome.debugger.onEvent.hasListeners()).toBeFalse()
+  })
+
+  it('should call subscribed callback', async () => {
+    chrome.debugger.getTargets.mockImplementation((callback) =>
+      callback([target])
+    )
+
+    await Requests.listen()
+    const listener = jest.fn()
+    const unsubscribe = Requests.subscribe(listener)
+
+    expect(listener).not.toBeCalled()
+
+    const event = { request: 'toto' }
+    chrome.debugger.onEvent.callListeners(
+      { targetId: target.id },
+      'Fetch.requestPaused',
+      event
+    )
+    expect(listener).toBeCalledWith('toto')
+
+    unsubscribe()
+  })
+})
