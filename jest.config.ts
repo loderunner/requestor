5aadcc64cd5e98b947c4163f92ddece55baa52ea
Charles Francoise <charles.francoise@payfit.com>
2022-02-08 21:57:27 +0100

    fix resolver for tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 /// <reference types="jest-extended" />
+
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['jest-extended/all', './src/__setups__/chrome.setup.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 }
