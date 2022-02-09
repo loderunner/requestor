7ae2728a061e1d5b34e847f940fabb381b42556d
Charles Francoise <charles.francoise@payfit.com>
2022-02-09 13:53:06 +0100

    allow for a more customized list header

--- src/panel/components/List.tsx
+++ src/panel/components/List.tsx
@@ -6,11 +6,17 @@ interface Props {
   header?: React.ReactNode
 }
 
-const List = ({ className, header = '', items = [] }: Props) => (
-  <div className={`flex flex-col ${className}`}>
-    <div className="font-bold bg-slate-100">{header}</div>
-    <div className="overflow-y-auto">{items}</div>
-  </div>
-)
+const List = ({ className, header = '', items = [] }: Props) => {
+  if (typeof header === 'string') {
+    header = <div className="font-bold bg-slate-100">{header}</div>
+  }
+
+  return (
+    <div className={`flex flex-col ${className}`}>
+      {header}
+      <div className="overflow-y-auto">{items}</div>
+    </div>
+  )
+}
 
 export default List
