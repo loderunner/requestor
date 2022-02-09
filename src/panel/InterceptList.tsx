167570a26349a41628938fe347b75e0ffa634b30
Charles Francoise <charles.francoise@payfit.com>
2022-02-09 16:46:45 +0100

    add intercepts with plus button

--- src/panel/InterceptList.tsx
+++ src/panel/InterceptList.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
 import { useMemo, useState } from 'react'
 
+import { Plus as PlusIcon } from '@/icons'
 import * as intercept from '@/intercept'
 
 import List from './components/List'
+
+interface ItemProps {
+  inter: intercept.Intercept
+}
+
+const Item = ({ inter }: ItemProps) => (
+  <div className="select-none whitespace-nowrap overflow-hidden text-ellipsis">
+    <span>{inter.pattern}</span>
+  </div>
+)
 
 interface Props {
   className?: string
@@ -17,21 +28,27 @@ const InterceptList = ({ className }: Props) => {
   }
 
   const items = useMemo(
-    () => intercepts.map((inter, i) => <InterceptItem key={i} inter={inter} />),
+    () => intercepts.map((inter, i) => <Item key={i} inter={inter} />),
     [intercepts]
   )
 
-  return <List className={className} header="Intercepts" items={items}></List>
-}
+  const header = useMemo(
+    () => (
+      <div className="p-1 flex justify-between select-none bg-slate-100">
+        <span className="font-bold">Intercepts</span>
+        <button
+          className="self-stretch"
+          title="Add intercept"
+          onClick={() => addIntercept('gql')}
+        >
+          <PlusIcon className="h-full w-auto" />
+        </button>
+      </div>
+    ),
+    []
+  )
 
-interface ItemProps {
-  inter: intercept.Intercept
+  return <List className={className} header={header} items={items}></List>
 }
-
-const InterceptItem = ({ inter }: ItemProps) => (
-  <div className="whitespace-nowrap overflow-hidden text-ellipsis">
-    <span>{inter.pattern}</span>
-  </div>
-)
 
 export default InterceptList
