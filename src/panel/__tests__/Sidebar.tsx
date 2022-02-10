e747094db3ac0ad7bbe24c8f0897b550713e22e0
Charles Francoise <charles.francoise@payfit.com>
2022-02-10 16:07:15 +0100

    add missing snapshot tests

---
+++ src/panel/__tests__/Sidebar.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { cleanup, render } from '@testing-library/react'
+import * as React from 'react'
+import '@testing-library/jest-dom'
+
+import * as Intercept from '@/intercept'
+
+import Sidebar from '../Sidebar'
+
+// mock request
+const request: Intercept.Request = {
+  headers: {},
+  initialPriority: 'Medium',
+  method: 'GET',
+  referrerPolicy: 'same-origin',
+  url: 'https://example.com',
+}
+
+describe('[Sidebar]', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should match empty snapshot', () => {
+    const { container } = render(<Sidebar requests={[]} />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should match requestful snapshot', async () => {
+    const { container } = render(<Sidebar requests={[request]} />)
+    expect(container).toMatchSnapshot()
+  })
+})
