f4573fa5b3ca4e83ff47d267ac22e070dd6ecd48
Charles Francoise <charles.francoise@payfit.com>
2022-02-10 15:28:54 +0100

    add InterceptList snapshot tests

--- src/panel/__tests__/App.tsx
+++ src/panel/__tests__/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { cleanup, render, waitFor } from '@testing-library/react'
-import React from 'react'
+import * as React from 'react'
 import '@testing-library/jest-dom'
 
 import * as Intercept from '@/intercept'
