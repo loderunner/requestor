2d1374815418b85cf48932e66cf71333ed34d6de
Charles Francoise <charles.francoise@payfit.com>
2022-02-11 13:10:56 +0100

    select main view according to selection type

--- src/panel/Main.tsx
+++ src/panel/Main.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react'
 
+import { useSelection } from './selection'
 import Welcome from './Welcome'
 
 interface Props {
   className?: string
 }
 
-const Main = ({ className }: Props) => (
-  <main id="main" className={`min-h-screen ${className}`}>
-    <Welcome />
-  </main>
-)
+const Main = ({ className }: Props) => {
+  const [, , selectionType] = useSelection()
+
+  let view
+  switch (selectionType) {
+    case 'null':
+      view = <Welcome />
+      break
+    case 'intercept':
+      view = 'Not implemented yet'
+      break
+    case 'request':
+      view = 'Not implemented yet'
+      break
+  }
+
+  return (
+    <main id="main" className={`min-h-screen ${className}`}>
+      {view}
+    </main>
+  )
+}
 
 export default Main
