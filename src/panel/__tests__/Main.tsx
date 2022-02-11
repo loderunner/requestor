14b6a7b65b3685b52d502cfcd79e56bf67a8a905
Charles Francoise <charles.francoise@payfit.com>
2022-02-11 13:41:39 +0100

    update tests

--- src/panel/__tests__/Main.tsx
+++ src/panel/__tests__/Main.tsx
@@ -4,13 +4,48 @@ import * as React from 'react'
 import '@testing-library/jest-dom'
 
 import Main from '../Main'
+import * as selection from '../selection'
+
+jest.mock('../selection')
+const mockedSelection = selection as jest.Mocked<typeof selection>
 
 describe('[Main]', () => {
   afterEach(() => {
     cleanup()
   })
 
-  it('should match empty snapshot', () => {
+  it('should match empty selection snapshot', () => {
+    mockedSelection.useSelection.mockImplementation(() => [
+      null,
+      () => {},
+      'null',
+    ])
+    const { container } = render(<Main />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should match intercept selection snapshot', () => {
+    mockedSelection.useSelection.mockImplementation(() => [
+      { pattern: 'helloworld', enabled: true },
+      () => {},
+      'intercept',
+    ])
+    const { container } = render(<Main />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should match request selection snapshot', () => {
+    mockedSelection.useSelection.mockImplementation(() => [
+      {
+        headers: {},
+        initialPriority: 'Medium',
+        method: 'GET',
+        referrerPolicy: 'same-origin',
+        url: 'https://example.com',
+      },
+      () => {},
+      'request',
+    ])
     const { container } = render(<Main />)
     expect(container).toMatchSnapshot()
   })
