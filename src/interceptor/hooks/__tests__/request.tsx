7ba361ce003973e91a4791c34e287ef7b30804e7
Charles Francoise <charles.francoise@payfit.com>
2022-02-18 22:09:26 +0100

    add request hooks

---
+++ src/interceptor/hooks/__tests__/request.tsx
@@ -0,0 +1,111 @@
+import {
+  act as actHook,
+  cleanup as cleanupHooks,
+  renderHook,
+} from '@testing-library/react-hooks'
+import * as React from 'react'
+
+import { RequestProvider, useRequest, useRequests } from '..'
+import * as Interceptor from '../../request'
+
+import type { RequestEventListener } from '../../request'
+
+jest.mock('../../request')
+const mockedInterceptor = Interceptor as jest.Mocked<typeof Interceptor>
+
+// mock request
+const request: Interceptor.Request = {
+  id: 'request',
+  headers: {},
+  initialPriority: 'Medium',
+  method: 'GET',
+  referrerPolicy: 'same-origin',
+  url: 'https://example.com',
+}
+
+describe('[RequestHooks.useRequests]', () => {
+  afterEach(() => {
+    cleanupHooks()
+  })
+
+  it('should throw without a RequestProvider', () => {
+    const { result } = renderHook(() => useRequests())
+    expect(result.error).toBeDefined()
+  })
+
+  it('should return an empty array', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <RequestProvider>{children}</RequestProvider>
+    )
+    const { result } = renderHook(() => useRequests(), { wrapper })
+
+    expect(result.current).toBeArrayOfSize(0)
+  })
+
+  it('should add a request to the list on listener', async () => {
+    let listener: RequestEventListener = () => {
+      throw new Error('listener called before subscribe')
+    }
+    mockedInterceptor.subscribe.mockImplementation((l) => {
+      listener = l
+      return jest.fn()
+    })
+    mockedInterceptor.pushRequest.mockImplementation((req) =>
+      Interceptor.requests.push(req)
+    )
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <RequestProvider>{children}</RequestProvider>
+    )
+    const { result } = renderHook(() => useRequests(), { wrapper })
+    await actHook(() => {
+      mockedInterceptor.pushRequest(request)
+      listener(request)
+    })
+
+    expect(result.current).toBeArrayOfSize(1)
+    expect(result.current).toContain(request)
+  })
+})
+
+describe('[RequestHooks.useRequest]', () => {
+  afterEach(() => {
+    cleanupHooks()
+  })
+
+  it('should throw without a RequestProvider', () => {
+    const { result } = renderHook(() => useRequest('request'))
+    expect(result.error).toBeDefined()
+  })
+
+  it('should throw on missing request', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <RequestProvider>{children}</RequestProvider>
+    )
+    const { result } = renderHook(() => useRequest('toto'), { wrapper })
+
+    expect(result.error).toBeDefined()
+  })
+
+  it('should add a request to the list on listener', async () => {
+    let listener: RequestEventListener = () => {
+      throw new Error('listener called before subscribe')
+    }
+    mockedInterceptor.subscribe.mockImplementation((l) => {
+      listener = l
+      return jest.fn()
+    })
+    mockedInterceptor.pushRequest.mockImplementation((req) =>
+      Interceptor.requests.push(req)
+    )
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <RequestProvider>{children}</RequestProvider>
+    )
+    const { result } = renderHook(() => useRequest('request'), { wrapper })
+    await actHook(() => {
+      mockedInterceptor.pushRequest(request)
+      listener(request)
+    })
+
+    expect(result.current).toEqual(request)
+  })
+})
---
+++ src/interceptor/hooks/request.tsx
@@ -0,0 +1,68 @@
+import {
+  Provider as JotaiProvider,
+  atom,
+  useAtom,
+  useAtomValue,
+  useSetAtom,
+} from 'jotai'
+import React, { useLayoutEffect } from 'react'
+
+import * as Interceptor from '../request'
+
+const requestScope = Symbol('RequestProviderScope')
+
+interface Props {
+  children: React.ReactNode
+}
+
+const requestsAtom = atom<Interceptor.Request[] | undefined>(undefined)
+
+// Children wrapper to use jotai hooks inside Provider scope
+const Children = ({ children }: Props) => {
+  const setRequests = useSetAtom(requestsAtom, requestScope)
+  useLayoutEffect(() => {
+    return Interceptor.subscribe(() => {
+      setRequests([...Interceptor.requests])
+    })
+  }, [])
+  return <>{children}</>
+}
+
+export const RequestProvider = ({ children }: Props) => {
+  return (
+    <JotaiProvider
+      scope={requestScope}
+      initialValues={[[requestsAtom, [...Interceptor.requests]]]}
+    >
+      <Children>{children}</Children>
+    </JotaiProvider>
+  )
+}
+
+export const useRequests = () => {
+  const requests = useAtomValue(requestsAtom, requestScope)
+
+  if (requests === undefined) {
+    throw new Error(
+      'no requests in scope\nDid you call useRequests inside a RequestProvider?'
+    )
+  }
+
+  return requests as ReadonlyArray<Readonly<Interceptor.Request>>
+}
+
+export const useRequest = (id: string) => {
+  const requests = useAtomValue(requestsAtom, requestScope)
+  if (requests === undefined) {
+    throw new Error(
+      'no requests in scope\nDid you call useRequest inside a RequestProvider?'
+    )
+  }
+
+  const request = requests.find((r) => r.id === id)
+  if (request === undefined) {
+    throw new Error('request not found')
+  }
+
+  return request as Readonly<Interceptor.Request>
+}
