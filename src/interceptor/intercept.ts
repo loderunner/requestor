c3d448992cf0ab7fd1421d7d11aa5169d723d8be
Charles Francoise <charles.francoise@payfit.com>
2022-02-21 22:53:05 +0100

    update intercept in model

--- src/interceptor/intercept.ts
+++ src/interceptor/intercept.ts
@@ -28,6 +28,7 @@ export const updateIntercept = (
   if (current !== undefined) {
     current.pattern = inter.pattern ?? current.pattern
     current.enabled = inter.enabled ?? current.enabled
+    current.regexp = inter.regexp ?? current.regexp
   }
   return current
 }
