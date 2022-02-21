c430a8d6931fd05c494ed06b3654c48739ac46b3
Charles Francoise <charles.francoise@payfit.com>
2022-02-21 13:28:14 +0100

    fix intercept hooks mock

--- src/interceptor/hooks/__mocks__/intercept.ts
+++ src/interceptor/hooks/__mocks__/intercept.ts
@@ -1,10 +1,16 @@
+const intercepts = [globalMocks.intercept]
+const addIntercept = jest.fn(() => globalMocks.intercept)
+const removeIntercept = jest.fn(() => {})
+
 export const useIntercepts = jest.fn(() => ({
-  intercepts: [globalMocks.intercept],
-  addIntercept: jest.fn(() => globalMocks.intercept),
-  removeIntercept: jest.fn(() => {}),
+  intercepts,
+  addIntercept,
+  removeIntercept,
 }))
+
+const updateIntercept = jest.fn(() => {})
 
 export const useIntercept = jest.fn(() => ({
   intercept: globalMocks.intercept,
-  updateIntercept: jest.fn(() => {}),
+  updateIntercept,
 }))
