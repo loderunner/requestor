8dad1375056045e5b9c875ff694f587f84bc2e82
Charles Francoise <charles.francoise@payfit.com>
2022-02-22 21:18:11 +0100

    failRequest on debugger level

--- src/interceptor/__tests__/request.ts
+++ src/interceptor/__tests__/request.ts
@@ -1,7 +1,7 @@
 import { chrome } from 'jest-chrome'
 
 import { listen, unlisten } from '..'
-import { continueRequest, pushRequest, requests } from '../request'
+import { continueRequest, failRequest, pushRequest, requests } from '../request'
 
 describe('[Interceptor.pushRequest]', () => {
   beforeEach(async () => {
@@ -52,6 +52,40 @@ describe('[Interceptor.continueRequest]', () => {
     expect(requests).toBeArrayOfSize(1)
 
     await continueRequest(globalMocks.request.id)
+    expect(requests).toBeArrayOfSize(0)
+  })
+})
+
+describe('[Interceptor.failRequest]', () => {
+  beforeEach(async () => {
+    chrome.debugger.getTargets.mockImplementation((callback) =>
+      callback([globalMocks.target])
+    )
+    await listen()
+    jest.clearAllMocks()
+  })
+
+  afterEach(() => {
+    unlisten()
+  })
+
+  it('should call debugger', async () => {
+    await failRequest(globalMocks.request.id)
+    expect(chrome.debugger.sendCommand).toHaveBeenCalledWith(
+      expect.objectContaining({ targetId: globalMocks.target.id }),
+      'Fetch.failRequest',
+      expect.objectContaining({
+        requestId: globalMocks.request.id,
+        errorReason: 'Aborted',
+      })
+    )
+  })
+
+  it('should remove request from requests list', async () => {
+    pushRequest(globalMocks.request)
+    expect(requests).toBeArrayOfSize(1)
+
+    await failRequest(globalMocks.request.id)
     expect(requests).toBeArrayOfSize(0)
   })
 })
