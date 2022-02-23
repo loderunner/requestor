8502d06108d552b056e4df4e89b90b0b2dc7f3f4
Charles Francoise <charles.francoise@payfit.com>
2022-02-23 23:06:49 +0100

    fix import order

--- src/panel/components/body-view/__tests__/PlainText.tsx
+++ src/panel/components/body-view/__tests__/PlainText.tsx
@@ -1,7 +1,7 @@
 import { cleanup, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import * as React from 'react'
 import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
 
 import PlainTextBodyView from '../PlainText'
 
