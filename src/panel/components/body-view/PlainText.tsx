af1b7603e059f2755e1b589b631da2990d22ff10
Charles Francoise <charles.francoise@payfit.com>
2022-02-23 21:08:37 +0100

    styled and editable PlainTextBodyView calls onChange callback

--- src/panel/components/body-view/PlainText.tsx
+++ src/panel/components/body-view/PlainText.tsx
@@ -1,18 +1,42 @@
 import * as React from 'react'
+import { useCallback, useLayoutEffect, useState } from 'react'
 
 interface Props {
   data?: string
-  onInput?: React.ReactEventHandler
+  onChange?: (body?: string) => void
 }
 
-const PlainTextBodyView = ({ data = '', onInput }: Props) => (
-  <pre
-    className={`bg-slate-100 px-6 py-6 whitespace-pre-wrap break-all`}
-    contentEditable="true"
-    onInput={onInput}
-  >
-    {data}
-  </pre>
-)
+const PlainTextBodyView = ({ data, onChange }: Props) => {
+  const [bodyData, setBodyData] = useState(data)
+
+  useLayoutEffect(() => setBodyData(data), [data])
+
+  const onInput = useCallback(
+    (e: React.SyntheticEvent<HTMLPreElement, FocusEvent>) => {
+      setBodyData(e.currentTarget.textContent ?? undefined)
+      if (onChange !== undefined) {
+        onChange(e.currentTarget.textContent ?? undefined)
+      }
+    },
+    [onChange]
+  )
+
+  console.log('PlainTextBodyView render', { data, bodyData })
+
+  return React.useMemo(
+    () => (
+      <pre
+        className="bg-slate-100 px-6 py-6 whitespace-pre-wrap break-all"
+        contentEditable="true"
+        suppressContentEditableWarning
+        role="textbox"
+        onBlur={onInput}
+      >
+        {bodyData}
+      </pre>
+    ),
+    [bodyData, onInput]
+  )
+}
 
 export default PlainTextBodyView
