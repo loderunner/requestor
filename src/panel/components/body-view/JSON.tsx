b9480f875def6e7dd10ec3a686240ed5cc7fdec6
Charles Francoise <charles.francoise@payfit.com>
2022-02-24 22:51:26 +0100

    add basic JSON editor

--- src/panel/components/body-view/JSON.tsx
+++ src/panel/components/body-view/JSON.tsx
@@ -3,16 +3,16 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import { MoreHoriz as MoreHorizIcon } from '@/icons'
 
-import type { CSSProperties, MutableRefObject } from 'react'
+import ModalInput from '../ModalInput'
 
-type JSONValue = null | boolean | number | string | JSONArray | JSONObject
+import type { CSSProperties, MutableRefObject, SyntheticEvent } from 'react'
 
 type JSONArray = JSONValue[]
 type JSONObject = { [key: string]: JSONValue }
+type JSONPrimitive = null | boolean | number | string
+type JSONValue = JSONPrimitive | JSONArray | JSONObject
 
-type PrimitiveValue = null | boolean | number | string
-
-const isPrimitive = (value: JSONValue): value is PrimitiveValue =>
+const isPrimitive = (value: JSONValue): value is JSONPrimitive =>
   typeof value === 'boolean' ||
   typeof value === 'number' ||
   typeof value === 'string' ||
@@ -22,49 +22,80 @@ const isObject = (value: JSONValue): value is JSONObject =>
   value instanceof Object
 
 interface PrimitiveProps {
-  value: PrimitiveValue
+  value: JSONPrimitive
+  onChange: (value: JSONPrimitive) => void
 }
 
-const PrimitiveView = ({ value }: PrimitiveProps) => {
-  switch (typeof value) {
-    case 'boolean':
-      return (
-        <pre className="whitespace-pre-wrap break-all text-green-500">
-          {JSON.stringify(value)}
-        </pre>
-      )
-    case 'number':
-      return (
-        <pre className="whitespace-pre-wrap break-all text-rose-500">
-          {JSON.stringify(value)}
-        </pre>
-      )
-    case 'string':
-      return (
-        <pre className="whitespace-pre-wrap break-all text-sky-400">
-          {JSON.stringify(value)}
-        </pre>
-      )
-    case 'object': {
-      if (value === null) {
-        return (
-          <pre className="whitespace-pre-wrap break-all text-purple-500">
-            {JSON.stringify(value)}
-          </pre>
-        )
-      }
+const PrimitiveView = ({ value, onChange }: PrimitiveProps) => {
+  const preRef = useRef<HTMLPreElement>(null)
+  const [editing, setEditing] = useState(false)
+
+  const onDoubleClick = useCallback((e: SyntheticEvent) => {
+    if (!preRef.current) {
+      return
     }
-  }
-  throw new Error('invalid JSON value')
+    e.stopPropagation()
+    setEditing(true)
+  }, [])
+
+  const onChangeInput = useCallback(
+    (value) => {
+      onChange(JSON.parse(value))
+      setEditing(false)
+    },
+    [onChange]
+  )
+
+  const onCancelInput = useCallback(() => setEditing(false), [])
+
+  const textColor = useMemo(() => {
+    switch (typeof value) {
+      case 'boolean':
+        return 'text-green-500'
+      case 'number':
+        return 'text-rose-500'
+      case 'string':
+        return 'text-sky-400'
+      case 'object': {
+        if (value === null) {
+          return 'text-purple-500'
+        }
+      }
+      // fall through
+      default:
+        throw new Error('invalid JSON value')
+    }
+  }, [value])
+
+  return (
+    <div className="flex-auto">
+      <pre
+        className={`whitespace-pre-wrap break-all ${textColor}`}
+        ref={preRef}
+        onDoubleClick={onDoubleClick}
+      >
+        {JSON.stringify(value)}
+      </pre>
+      {editing && preRef.current ? (
+        <ModalInput
+          element={preRef.current}
+          value={JSON.stringify(value)}
+          onChange={onChangeInput}
+          onCancel={onCancelInput}
+        />
+      ) : null}
+    </div>
+  )
 }
 
 interface ObjectProps {
   obj: JSONObject | JSONArray
   depth: number
   foldButtonRef?: MutableRefObject<HTMLButtonElement | null>
+  onChange: (obj: JSONObject | JSONArray) => void
 }
 
-const ObjectView = ({ obj, depth, foldButtonRef }: ObjectProps) => {
+const ObjectView = ({ obj, depth, foldButtonRef, onChange }: ObjectProps) => {
   const [folded, setFolded] = useState(depth > 1)
   const style: CSSProperties = useMemo(
     () => ({ marginLeft: `${depth}rem` }),
@@ -86,17 +117,26 @@ const ObjectView = ({ obj, depth, foldButtonRef }: ObjectProps) => {
         items.push(
           <div key={k} className="flex items-baseline space-x-1" style={style}>
             <pre className="text-amber-700">{k}:</pre>
-            <PrimitiveView value={v} />
+            <PrimitiveView
+              value={v}
+              onChange={(newV) => onChange({ ...obj, [k]: newV })}
+            />
           </div>
         )
       } else if (isArray(v) || isObject(v)) {
-        items.push(<ObjectRows key={k} {...{ k, v, style, depth }} />)
+        items.push(
+          <ObjectRows
+            key={k}
+            {...{ k, v, style, depth }}
+            onChange={(newV) => onChange({ ...obj, [k]: newV })}
+          />
+        )
       } else {
         throw new Error('invalid JSON value')
       }
     }
     return items
-  }, [obj, style, depth])
+  }, [obj, style, onChange, depth])
 
   return <div className={'space-y-1' + (folded ? ' hidden' : '')}>{rows}</div>
 }
@@ -106,9 +146,10 @@ interface RowsProps {
   v: JSONObject | JSONArray
   style: CSSProperties
   depth: number
+  onChange: (v: JSONValue | JSONArray) => void
 }
 
-const ObjectRows = ({ k, v, style, depth }: RowsProps) => {
+const ObjectRows = ({ k, v, style, depth, onChange }: RowsProps) => {
   const buttonRef = useRef<HTMLButtonElement | null>(null)
 
   const [leftGuard, rightGuard] = useMemo(() => {
@@ -144,25 +185,40 @@ const ObjectRows = ({ k, v, style, depth }: RowsProps) => {
           {rightGuard}
         </pre>
       </div>
-      <ObjectView obj={v} depth={depth + 1} foldButtonRef={buttonRef} />
+      <ObjectView
+        obj={v}
+        depth={depth + 1}
+        foldButtonRef={buttonRef}
+        onChange={(newV) => onChange(newV)}
+      />
     </>
   )
 }
 
 interface Props {
   jsonData: string
+  onChange?: (body?: string) => void
 }
 
-const JSONBodyView = ({ jsonData }: Props) => {
-  const value = useMemo<JSONValue>(() => JSON.parse(jsonData), [jsonData])
+const JSONBodyView = ({ jsonData, onChange }: Props) => {
+  const [value, setValue] = useState<JSONValue>(() => JSON.parse(jsonData))
+  const onBodyChange = useCallback(
+    (newValue: JSONValue) => {
+      setValue(newValue)
+      if (onChange !== undefined) {
+        onChange(JSON.stringify(newValue))
+      }
+    },
+    [onChange]
+  )
   const view = useMemo(() => {
     if (isPrimitive(value)) {
-      return <PrimitiveView value={value} />
+      return <PrimitiveView value={value} onChange={onBodyChange} />
     } else if (isArray(value) || isObject(value)) {
-      return <ObjectView obj={value} depth={0} />
+      return <ObjectView obj={value} depth={0} onChange={onBodyChange} />
     }
     throw new Error('invalid JSON value')
-  }, [value])
+  }, [onBodyChange, value])
   return view
 }
 
